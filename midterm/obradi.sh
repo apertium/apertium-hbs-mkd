2de4fec3aa45dbea2053c2d687a0245d7a12ae82
Hrvoje Peradin <hperadin@gmail.com>
2011-06-28 22:39:26 +0000

    Down to 20ish unknown words :)

--- midterm/obradi.sh
+++ midterm/obradi.sh
@@ -1,2 +1,2 @@
 cat article.sh.txt | apertium -d .. sh-mk-morph | python ../tagger-to-visl.py > article.sh.analysed.txt
-grep "ukjent" article.sh.analysed.txt | sed 's_ukjent__' > unknown.txt
+grep "ukjent" article.sh.analysed.txt | sed 's_ukjent__' |sort -u > unknown.txt
