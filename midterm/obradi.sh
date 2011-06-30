7920113486c391139daf4e368696209b7c8c79f9
Hrvoje Peradin <hperadin@gmail.com>
2011-06-30 23:05:42 +0000

    svašta nešto

--- midterm/obradi.sh
+++ midterm/obradi.sh
@@ -1,2 +1,2 @@
-cat article.sh.txt | apertium -d .. sh-mk-morph | python ../tagger-to-visl.py > article.sh.analysed.txt
+cat article.sh.txt | apertium -d .. sh-mk | python ../tagger-to-visl.py > article.sh.analysed.txt
 grep "ukjent" article.sh.analysed.txt | sed 's_ukjent__' |sort -u > unknown.txt
