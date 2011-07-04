2058f8a25d8ebc547ca75a482107becfd6a849e8
Hrvoje Peradin <hperadin@gmail.com>
2011-07-04 17:31:22 +0000

    some initial experiments with CG grammar rules

--- midterm/obradi.sh
+++ midterm/obradi.sh
@@ -1,2 +1,3 @@
-cat article.sh.txt | apertium -d .. sh-mk | python ../tagger-to-visl.py > article.sh.analysed.txt
-grep "ukjent" article.sh.analysed.txt | sed 's_ukjent__' |sort -u > unknown.txt
+cat article.sh.txt | apertium -d .. sh-mk-morph | python ../tagger-to-visl.py | vislcg3 --trace --grammar ../apertium-sh-mk.sh-mk.rlx > article.sh.analysed.txt
+# | python ../tagger-to-visl.py 
+# grep "ukjent" article.sh.analysed.txt | sed 's_ukjent__' |sort -u > unknown.txt
