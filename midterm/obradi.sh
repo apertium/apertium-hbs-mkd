4b1b96113c411f6ba75d609d1923241462fe2fd7
Hrvoje Peradin <hperadin@gmail.com>
2011-07-11 19:53:52 +0000

    tiny corrections

--- midterm/obradi.sh
+++ midterm/obradi.sh
@@ -1,4 +1,4 @@
-cat article.sh.txt | apertium -d .. sh-mk  > article.sh.analysed.txt
-#| python ../tagger-to-visl.py | vislcg3 --trace --grammar ../apertium-sh-mk.sh-mk.rlx > article.sh.analysed.txt
+cat article.sh.txt | apertium -d .. sh-mk > article.sh.analysed.txt
+#  | python ../tagger-to-visl.py | vislcg3 --trace --grammar ../apertium-sh-mk.sh-mk.rlx > article.sh.analysed.txt
 # | python ../tagger-to-visl.py 
 # grep "ukjent" article.sh.analysed.txt | sed 's_ukjent__' |sort -u > unknown.txt
