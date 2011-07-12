62505bbbd05572d0ce6d674b0167713da2f6bfb8
Hrvoje Peradin <hperadin@gmail.com>
2011-07-12 20:37:52 +0000

    a couple of new transfer rules, and additions to the dixes

--- midterm/obradi.sh
+++ midterm/obradi.sh
@@ -1,4 +1,5 @@
-cat article.sh.txt | apertium -d .. sh-mk-chunker > article.sh.analysed.txt
-#  | python ../tagger-to-visl.py | vislcg3 --trace --grammar ../apertium-sh-mk.sh-mk.rlx > article.sh.analysed.txt
+cat article.sh.txt | apertium -d .. sh-mk > article.sh.analysed.txt
+cat article.sh.txt | apertium -d .. sh-mk-chunker > article.sh.chunker.txt
+cat article.sh.txt | apertium -d .. sh-mk-morph | python ../tagger-to-visl.py | vislcg3 --trace --grammar ../apertium-sh-mk.sh-mk.rlx > article.sh.morph.txt
 # | python ../tagger-to-visl.py 
 # grep "ukjent" article.sh.analysed.txt | sed 's_ukjent__' |sort -u > unknown.txt
