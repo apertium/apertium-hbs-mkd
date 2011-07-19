1564e1a3843e1951d0b6885a48c62c80acec8130
Francis M. Tyers <ftyers@prompsit.com>
2011-07-19 11:44:19 +0000

    update file

--- dev/inconsistency.sh
+++ dev/inconsistency.sh
@@ -2,7 +2,7 @@ TMPDIR=/tmp
 
 if [[ $1 = "mk-sh" ]]; then
 
-lt-expand ../apertium-sh-mk.mk.dix | grep -v '<prn><enc>' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-sh-mk.mk.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../apertium-sh-mk.mk-sh.t1x  ../mk-sh.t1x.bin  ../mk-sh.autobil.bin |
         apertium-interchunk ../apertium-sh-mk.mk-sh.t2x  ../mk-sh.t2x.bin |
@@ -12,7 +12,7 @@ paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc
 
 elif [[ $1 = "sh-mk" ]]; then
 
-lt-expand ../apertium-sh-mk.sh.dix | grep -v '<prn><enc>' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-sh-mk.sh.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../apertium-sh-mk.sh-mk.t1x  ../sh-mk.t1x.bin  ../sh-mk.autobil.bin |
         apertium-interchunk ../apertium-sh-mk.sh-mk.t2x  ../sh-mk.t2x.bin |
