0065902f4cccf153748aab691711e63bbeb9af5c
Hrvoje Peradin <hperadin@gmail.com>
2011-08-07 18:29:30 +0000

    @Adv← rule for partitive after adverb, nouns that are candidates for the partitive from the analyser

--- dev/n/existing.sh
+++ dev/n/existing.sh
@@ -3,4 +3,4 @@
 #First to kill off the yat differences:
 cat uncategorised.txt | sed "s:iě:ije:" | sed "s:jěi:je:" | sed "s:ěi:i:" | sed "s:ě:je:" | sed  -e "s:+nt::" -e "s:+mi::" -e "s:+ma::" -e "s:+f::"> popišček.txt
 
-lt-expand ../../apertium-sh-mk.sh.dix | sed -e "s|<.*>||g" -e "s|.*:||" | sort -u | grep --file=popišček.txt -w > existing.txt.tmp
+lt-expand ../../apertium-sh-mk.sh.dix | sed -e "s|<.*>||g" -e "s|.*:||" | sort -u | grep --file=popišček.txt -w > existing.txt.tmpx
