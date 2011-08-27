857ff96159999de200aec9ae699345a8c9310345
Hrvoje Peradin <hperadin@gmail.com>
2011-08-27 13:12:39 +0000

    some cleanup

--- dev/n/existing.sh
+++
@@ -1,6 +0,0 @@
-# a script to see which of entries from uncategorised.txt already exist in the sh monodix
-
-#First to kill off the yat differences:
-cat uncategorised.txt | sed "s:iě:ije:" | sed "s:jěi:je:" | sed "s:ěi:i:" | sed "s:ě:je:" | sed  -e "s:+nt::" -e "s:+mi::" -e "s:+ma::" -e "s:+f::"> popišček.txt
-
-lt-expand ../../apertium-sh-mk.sh.dix | sed -e "s|<.*>||g" -e "s|.*:||" | sort -u | grep --file=popišček.txt -w > existing.txt.tmpx
